82d6aaad14bf911223a5747bcdcb471759f03642
“Debajyoti <wwwdeb888@gmail.com>
2025-04-30 20:26:48 +1000

    feat: fixed

--- stripetest/src/App.tsx
+++ stripetest/src/App.tsx
@@ -1,71 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { StripeElementsOptionsMode, loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import axios from "axios"; // Optional
 import CheckoutFormBoth from "./CheckoutFormBoth";
 const apiUrl = import.meta.env.VITE_STRIPE_PUB_KEY;
 const stripePromise = loadStripe(apiUrl);
 
 const App: React.FC = () => {
-  const [clientSecret, setClientSecret] = useState<string | null>(null);
-  const [clientSecretX, setClientSecretX] = useState<string | null>(null);
+  // const [clientSecret] = useState<string | null>(null);
+  // const [clientSecretX] = useState<string | null>(null);
   const [directDebit, setdirectDebit] = useState<boolean>(true);
 
 
 
   useEffect(() => {
     // Function to call the API
-    const fetchPaymentIntent = async () => {
-      try {
-        const response = await axios.post("https://stripetest-pl0s.onrender.com/create-payment-intent-express", {
-          amount: 100, // Amount in cents (e.g., $10.00)
-          currency: "usd", // Currency
-        });
-        setClientSecret(response.data.clientSecret);
-      } catch (error) {
-        console.error("Error creating payment intent:", error);
-      }
-    };
+    // const fetchPaymentIntent = async () => {
+    //   try {
+    //     const response = await axios.post("https://stripetest-pl0s.onrender.com/create-payment-intent-express", {
+    //       amount: 100, // Amount in cents (e.g., $10.00)
+    //       currency: "usd", // Currency
+    //     });
+    //     setClientSecret(response.data.clientSecret);
+    //   } catch (error) {
+    //     console.error("Error creating payment intent:", error);
+    //   }
+    // };
 
     // fetchPaymentIntent();
   }, []);
   useEffect(() => {
     // Function to call the API
-    const fetchPaymentIntentX = async () => {
-      try {
-        const response = await axios.post("https://stripetest-pl0s.onrender.com/create-setup-intent-express", {
-          customerId: "cus_RnanFK87tc6FxT", // Currency
-        });
-        setClientSecretX(response.data.clientSecret);
-      } catch (error) {
-        console.error("Error creating payment intent:", error);
-      }
-    };
+    // const fetchPaymentIntentX = async () => {
+    //   try {
+    //     const response = await axios.post("https://stripetest-pl0s.onrender.com/create-setup-intent-express", {
+    //       customerId: "cus_RnanFK87tc6FxT", // Currency
+    //     });
+    //     setClientSecretX(response.data.clientSecret);
+    //   } catch (error) {
+    //     console.error("Error creating payment intent:", error);
+    //   }
+    // };
 
     // fetchPaymentIntentX();
   }, []);
 
   // Options for the Elements provider
-  const options = clientSecret ? { clientSecret } : undefined;
   const optionsxx:StripeElementsOptionsMode = {
     mode: 'payment',
     amount: 1099,
     currency: 'usd',
     // Customizable with appearance API.
   };
-  const options1 = clientSecretX ? { clientSecret:clientSecretX,applePay: {
-    recurringPaymentRequest: {
-      paymentDescription: "Direct Debit Subscription",
-      regularBilling: {
-        amount: 0,
-        recurringPaymentIntervalUnit: "month",
-        recurringPaymentIntervalCount: 1,
-        label:"direct debit setup"
-      },
-      billingAgreement: "billing agreement",
-      managementURL: "https://stripe.com",
-    },
-  }, } : undefined;
+  // const options1 = clientSecretX ? { clientSecret:clientSecretX,applePay: {
+  //   recurringPaymentRequest: {
+  //     paymentDescription: "Direct Debit Subscription",
+  //     regularBilling: {
+  //       amount: 0,
+  //       recurringPaymentIntervalUnit: "month",
+  //       recurringPaymentIntervalCount: 1,
+  //       label:"direct debit setup"
+  //     },
+  //     billingAgreement: "billing agreement",
+  //     managementURL: "https://stripe.com",
+  //   },
+  // }, } : undefined;
 
 
   return (
@@ -85,12 +83,12 @@ const App: React.FC = () => {
       </button>
 
 
-      {clientSecret && !directDebit && (
+      {true  && (
         <Elements stripe={stripePromise} options={optionsxx}>
           <CheckoutFormBoth directDebit={directDebit}/>
         </Elements>
       )}
-      {clientSecretX && directDebit && (
+      {/* {clientSecretX && directDebit && (
         <Elements stripe={stripePromise} options={options1}>
           <CheckoutFormBoth directDebit={directDebit}/>
         </Elements>
@@ -100,7 +98,7 @@ const App: React.FC = () => {
           <p>Loading.......</p>
         )
 
-      }
+      } */}
 
     </div>
   );
